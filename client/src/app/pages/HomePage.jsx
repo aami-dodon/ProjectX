a9152ec06aa5380680ff8a73cc808f32212ba9e3
Sayantan Basu <62693630+sayabasu@users.noreply.github.com>
2025-10-25 07:02:36 +0530

    Refactor app pages and theme toggle

---
+++ client/src/app/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className="flex min-h-[calc(100vh-2*var(--space-xl))] items-center justify-center py-[calc(var(--space-xl)+var(--space-lg))]">
+    <h1 className="h1">Hello world</h1>
+  </div>
+);
+
+export default HomePage;
--- client/src/features/theme/index.js
+++ client/src/features/theme/index.js
@@ -1,3 +1,4 @@
 export { ThemeProvider } from './ThemeProvider';
 export { default as useTheme } from './useTheme';
+export { default as ThemeToggleCard } from './components/ThemeToggleCard';
 export * from './tokens';
