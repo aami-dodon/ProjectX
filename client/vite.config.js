cc7d87514303821d275298a523940e87e1375834
aami-dodon <aami@dodon.in>
2025-10-27 13:35:49 +0530

    doc

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 const port = parseInt(process.env.CLIENT_PORT) || 5173
 
@@ -13,18 +13,8 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    tsconfigPaths(), 
   ],
-  resolve: {
-    alias: [
-      { find: '@/layout', replacement: path.resolve(__dirname, './src/shared/components/layout') },
-      { find: '@/ui', replacement: path.resolve(__dirname, './src/shared/components/ui') },
-      { find: '@/lib', replacement: path.resolve(__dirname, './src/shared/lib') },
-      { find: '@/hooks', replacement: path.resolve(__dirname, './src/shared/hooks') },
-      { find: '@/components', replacement: path.resolve(__dirname, './src/shared/components/') },
-      { find: '@', replacement: path.resolve(__dirname, './src') },
-    ],
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-  },
   server: {
     port,
     host: true,
@@ -34,7 +24,6 @@ export default defineConfig({
       protocol: 'ws',
     },
   },
-  // Fix preload mismatch warning in some Cloudflare setups
   build: {
     rollupOptions: {
       output: {
