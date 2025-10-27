56d4cf16570fc0a3097c4b50b7f451fb69cefef6
aami-dodon <aami@dodon.in>
2025-10-27 12:49:30 +0530

    update

--- client/src/app/routes.jsx
+++ client/src/app/routes.jsx
@@ -2,6 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import { DashboardPage } from "@/features/dashboard/pages/DashboardPage";
 
+
+import BlankCenteredLayout from "./layouts/BlankCenteredLayout";
+import { LoginPage } from "@/features/auth/pages/LoginPage";
+
 export const router = createBrowserRouter([
   {
     element: <MainLayout />,
@@ -9,4 +13,6 @@ export const router = createBrowserRouter([
       { path: "/", element: <DashboardPage /> },
     ],
   },
+
+  
 ]);
