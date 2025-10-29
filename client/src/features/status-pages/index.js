bbc958fda026d556dc68231932a2ef7964d0662e
Sayantan Basu <62693630+sayabasu@users.noreply.github.com>
2025-10-30 01:55:08 +0530

    refactor: move status pages into feature module

---
+++ client/src/features/status-pages/index.js
@@ -0,0 +1,9 @@
+export { statusRoutes } from "./routes";
+
+export { ErrorPage } from "./pages/ErrorPage";
+export { ForbiddenPage } from "./pages/ForbiddenPage";
+export { InternalServerErrorPage } from "./pages/InternalServerErrorPage";
+export { NotFoundPage } from "./pages/NotFoundPage";
+export { RequestTimeoutPage } from "./pages/RequestTimeoutPage";
+export { ServiceUnavailablePage } from "./pages/ServiceUnavailablePage";
+export { UnauthorizedPage } from "./pages/UnauthorizedPage";
